6de18951d0685d2a3ae94c55326af42b2c41dfa4
Nathan Hardy <nhardy@live.com.au>
2017-09-21 11:29:41 +1200

    Clean main() structure

--- main.py
+++ main.py
@@ -7,18 +7,33 @@ Authors: Daniel Thomson, Levi Faid, Nathan Hardy, Rebecca Wilson
 
 import sys
 
+def result(width: int, length: int) -> int:
+    """
+    Returns the number of combinations of carpets possible for
+    a carpet of dimensions (width * length)
+    """
+
+    if width * length % 4 != 0:
+        return 0
+
+    return NotImplemented
+
 def main():
+    """
+    Interprets input according to specifications and produces the result
+    according to the output format defined by the etude
+    """
+
     for unstripped_line in sys.stdin.readlines():
         line = unstripped_line.strip()
 
         if line.startswith('#') or line == '':
             continue
 
+        width, length = map(int, line.split())
+
         print(line)
-
-        result = 0 # TODO: Actually do something
-
-        print('# {}'.format(result))
+        print('# {}'.format(result(width, length)))
 
 if __name__ == '__main__':
     main()
