1f7097a8c0fc5b8d1e2db8a5ab6e1e220560b097
Nathan Hardy <nhardy@live.com.au>
2017-09-21 11:57:35 +1200

    Add comments and plan for general structure of program

--- main.py
+++ main.py
@@ -6,6 +6,40 @@ Authors: Daniel Thomson, Levi Faid, Nathan Hardy, Rebecca Wilson
 """
 
 import sys
+from typing import Tuple
+
+class Grid:
+    """
+    Abstraction useful for serialising Grid state
+    """
+
+    def __init__(self, width: int, length: int):
+        self.width = width
+        self.length = length
+
+        self._grid = [[False for _ in range(length)] for _ in range(width)]
+
+    def fill(self, coords: Tuple[int, int]):
+        """
+        Sets the Grid cell at (x, y) as filled
+        """
+
+        x_pos, y_pos = coords
+        self._grid[x_pos][y_pos] = True
+
+    def serialise(self) -> int:
+        """
+        Returns a unique integer representing the state of Grid
+        """
+
+        total = 0
+
+        for i, row in enumerate(self._grid):
+            for j, cell in enumerate(row):
+                if cell:
+                    total += i * self.width + j
+
+        return total
 
 def result(width: int, length: int) -> int:
     """
@@ -13,10 +47,21 @@ def result(width: int, length: int) -> int:
     a carpet of dimensions (width * length)
     """
 
+    # Areas not divisible by 4 are impossible to fill
     if width * length % 4 != 0:
         return 0
 
-    return NotImplemented
+    # Dictionary of "Grid state" -> "No. of Combinations"
+    # This will be progressively built up by a depth-first search tree
+    # where if a given "Grid state" has already been reached, one
+    # can simply add the "No. of Combinations" instead of traversing
+    # further, if that value had already been calculated.
+    grid_states = {}
+
+    # TODO: Depth-First Search here
+
+    # The Final result will be stored in the empty Grid state
+    return grid_states[Grid(width, length).serialise()]
 
 def main():
     """
